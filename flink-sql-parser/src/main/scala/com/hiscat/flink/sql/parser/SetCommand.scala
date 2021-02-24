6a3be9fb46d4fd537dcee07f3a008b95b974e249
hiscat <1251723871@qq.com>
2021-02-24 23:28:58 +0800

    fix(flink-sql-parser): fix set command bug

--- flink-sql-parser/src/main/scala/com/hiscat/flink/sql/parser/SetCommand.scala
+++ flink-sql-parser/src/main/scala/com/hiscat/flink/sql/parser/SetCommand.scala
@@ -4,7 +4,7 @@ import org.apache.flink.table.api.{Table, TableEnvironment}
 
 case class SetCommand(sql: String) extends SqlCommand(sql) {
   override def call(env: TableEnvironment): Option[Table] = {
-    val split = sql.trim.split("=")
+    val split = sql.replace("SET","").trim.split("=")
     env.getConfig.getConfiguration.setString(split(0), split(1))
     None
   }
