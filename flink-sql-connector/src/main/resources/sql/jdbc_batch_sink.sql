56fe30aaaa2955ca6d6487ba08e7c4b6c1ded45c
谢波 <xiebo1@yonghui.cn>
2021-02-25 17:44:09 +0800

    feat(flink-sql-connector): add jdbc batch sink test

---
+++ flink-sql-connector/src/main/resources/sql/jdbc_batch_sink.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users
+(
+    id   BIGINT PRIMARY KEY NOT ENFORCED,
+    name VARCHAR(30)
+)WITH (
+     'connector' = 'jdbc',
+     'username' = 'root',
+     'password' = 'Yh002Yh002@',
+     'driver' = 'com.mysql.cj.jdbc.Driver',
+     'url' = 'jdbc:mysql://yh002:3306/flink',
+     'sink.buffer-flush.max-rows' = '1',
+     'sink.buffer-flush.interval' = '1s',
+     'sink.max-retries' = '3',
+--      'sink.parallelism' = '1', -- not support 1.12.1
+     'table-name' = 'users'
+     );
+
+INSERT INTO users
+SELECT 3,'flink';
--- flink-sql-connector/src/main/resources/sql/jdbc_scan.sql
+++ flink-sql-connector/src/main/resources/sql/jdbc_scan.sql
@@ -1,17 +1,22 @@
--- insert into users (id, name) values (1,'hello');
+-- insert into users (id, name) values (1,'hello'),(2,'world');
 CREATE TABLE users
 (
     id   BIGINT PRIMARY KEY NOT ENFORCED,
     name VARCHAR(30)
 )WITH (
-    'connector' = 'jdbc',
-    'username' = 'root',
-    'password' = 'Hdp10Hdp10@',
-    'driver' = 'com.mysql.cj.jdbc.Driver',
-    'url' = 'jdbc:mysql://hdp10:3306/flink',
-    'table-name' = 'users'
-);
-
+     'connector' = 'jdbc',
+     'username' = 'root',
+     'password' = 'Yh002Yh002@',
+     'driver' = 'com.mysql.cj.jdbc.Driver',
+     'url' = 'jdbc:mysql://yh002:3306/flink',
+     'scan.partition.column' = 'id',
+     'scan.partition.num' = '1',
+     'scan.partition.lower-bound' = '0',
+     'scan.partition.upper-bound' = '10000',
+     'scan.fetch-size' = '1',
+     'scan.auto-commit' = 'true',
+     'table-name' = 'users'
+     );
 CREATE TABLE print WITH('connector'='print') LIKE users(EXCLUDING ALL);
 
 INSERT INTO print SELECT * FROM users;
