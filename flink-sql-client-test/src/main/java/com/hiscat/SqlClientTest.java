321555b017557e68f4a2aad7c62a15f34383a1f9
hiscat <1251723871@qq.com>
2021-06-16 16:01:50 +0800

    add sql client url

--- flink-sql-client-test/src/main/java/com/hiscat/SqlClientTest.java
+++ flink-sql-client-test/src/main/java/com/hiscat/SqlClientTest.java
@@ -8,6 +8,7 @@ import org.apache.flink.table.client.SqlClient;
  *    add flink-conf.yaml execution.target: local
  * 2. restart idea
  * 3. just like normal sql-client.sh test
+ * https://ci.apache.org/projects/flink/flink-docs-release-1.13/docs/dev/table/sqlclient/#execute-a-set-of-sql-statements
  */
 public class SqlClientTest {
   public static void main(String[] args) {
