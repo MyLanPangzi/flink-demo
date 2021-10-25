03402204b13141a81c743865f5d17052542e64f7
xiebo03 <xiebo03@qutoutiao.net>
2021-10-25 15:40:00 +0800

    remove unused code

--- flink-hudi-test/src/main/resources/hive_sync.sh
+++
@@ -1,7 +0,0 @@
-./run_sync_tool.sh  --jdbc-url jdbc:hive2:\/\/yh001:10000 \
---user hdp \
---pass '' \
---partitioned-by partition \
---base-path hdfs://yh001:9820/hudi/orders \
---database default \
---table orders
