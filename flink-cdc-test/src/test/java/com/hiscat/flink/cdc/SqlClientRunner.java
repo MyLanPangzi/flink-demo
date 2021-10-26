0c9d9760ffb89d5e525891ecb27b6b8f0d340f1f
xiebo03 <xiebo03@qutoutiao.net>
2021-10-26 17:15:32 +0800

    cdc test

--- flink-cdc-test/src/test/java/com/hiscat/flink/cdc/SqlClientRunner.java
+++
@@ -1,9 +0,0 @@
-package com.hiscat.flink.cdc;
-
-import org.apache.flink.table.client.SqlClient;
-
-public class SqlClientRunner {
-    public static void main(String[] args) {
-        SqlClient.main(args);
-    }
-}
